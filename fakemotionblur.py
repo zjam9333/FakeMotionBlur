154a2142ccb32ae5f2e8ccc358029158827e81a1
zhangjingjian <zhangjingjian@dabby.cn>
2019-09-14 01:12:18 +0800

    beta, slow

--- fakemotionblur.py
+++ fakemotionblur.py
@@ -1,10 +1,12 @@
+# coding:utf-8
 import numpy as np
 from cv2 import cv2
 import math
+import os
 
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_video/py_lucas_kanade/py_lucas_kanade.html
 
-def blurKernel(length,angle):
+def blurKernel(length, angle):
     # 自制模糊核，长度和角度
     # 先假设y的正方向为上，需要时再翻转
     half = length / 2
@@ -56,121 +58,103 @@ def flowWithFrames(frame1, frame2):
     flow = cv2.calcOpticalFlowFarneback(prevF,nextF, None, 0.5, 3, 15, 3, 5, 1.2, 0)
     return flow
 
-def showFlow(flow):
-    # 转为极坐标，mag极径，ang极角
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-
-    hsv = np.array(np.zeros_like(frame1))
-    hsv[...,1] = 255
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    # vvv = hsv[...,2]
-
-    rgb = hsv.copy() #cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-    # newvv = rgb[...,2]
-    # thre = 0
-    # newvv[vvv > thre] = 255
-    # rgb[vvv <= thre] = 0
-
-    rgb = cv2.cvtColor(rgb, cv2.COLOR_HSV2BGR)
-
-    cv2.imshow("flow", rgb)
-
-def imgAdd(src1, src2, mask):
-    rows, cols, _ = src1.shape
+def imgAddBlur(src1, src2, mask):
+    # shape = src1.shape
     res = src1.copy()
+    mask3 = mask.astype(np.float)
+    locations = np.where(mask3 > 0)
 
-    # 框取有需要的范围
-    startrows = 0
-    endrows = rows - 1
-    for r in range(0,rows):
-        if startrows >= endrows:
-            break
-        row0 = mask[r, ...]
-        if row0.max() < 10:
-            startrows = r
-        row1 = mask[rows - 1 - r, ...]
-        if row1.max() < 10:
-            endrows = rows - r
-    if startrows > endrows:
-        return res
-
-    for r in range(startrows, endrows):
-        for c in range(cols):
-            val = mask[r, c]
-            if val == 0:
-                continue
-            percent = val / 255
-            po = src1[r, c]
-            pd = src2[r,c]
-            res[r, c] = po * (1 - percent) + pd * percent
+    val = mask3[locations]
+    percent = val * 2 / 255.0
+    percent[percent > 1] = 1
+    percent = percent.reshape(-1, 1)
+    po = src1[locations]
+    pd = src2[locations]
+    res[locations] = po * (1 - percent) + pd * percent
     return res
 
 def motionBlurWithFrames(frame1, frame2):
     flow = flowWithFrames(frame1, frame2)
     mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    test_round = 50
-    test_width = int(frame1.shape[1] / test_round)
-    test_height = int(frame1.shape[0] / test_round)
-    res = np.zeros_like(frame1)
-    # masksize = (frame1.shape[0], frame1.shape[1], 1)
+    test_round_x = 24
+    test_round_y = test_round_x / 2
+    test_width = int(frame1.shape[1] / test_round_x)
+    test_height = int(frame1.shape[0] / test_round_y)
+    # res = np.zeros_like(frame1)
+    res = frame1.copy()
     emptymask = np.zeros_like(frame1)
     emptymask = cv2.cvtColor(emptymask, cv2.COLOR_BGR2GRAY)
-    mask = emptymask.copy()
-    for in_x in range(test_round):
-        for in_y in range(test_round):
-            print('rounds:', in_x, in_y)
+    # mask = emptymask.copy()
+    for in_x in range(test_round_x):
+        for in_y in range(test_round_y):
             startx = in_x * test_width
             endx = startx + test_width
             starty = in_y * test_height
             endy = starty + test_height
-            mag_roi = mag[starty:endy, startx:endx]
-            ang_roi = ang[starty:endy, startx:endx]
-            if mag_roi.shape[0] == 0 or mag_roi.shape[1] == 0:
-                continue
-            mag_using = int(mag_roi.max())
-            if mag_using < 10:
-                continue
-            ang_using = int(np.average(ang_roi[mag_roi > 10].flatten()))
-            # # ang_using = int(-ang_using / np.pi * 180)
-            # mag_using = 20
-            # ang_using = 45
-            kernel = blurKernel(length = mag_using, angle = ang_using)
-            blur_ro = frame1.copy()
-            # blur_ro[starty:endy, startx:endx] = frame1[starty:endy, startx:endx]
-            blur_ro = cv2.filter2D(blur_ro,-1,kernel)
 
-            # res[starty:endy, startx:endx] = blur_roi
+            mag_roi_flat = mag[starty:endy, startx:endx].flatten()
+            mag_max = int(mag_roi_flat.max())
+            mag_using = int(np.average(mag_roi_flat[mag_roi_flat > (mag_max - 1)]))
+            mag_using = mag_using / 2
+            if mag_using < 1:
+                continue
+
+            ang_roi_flat = ang[starty:endy, startx:endx].flatten()
+            ang_using = int(np.average(ang_roi_flat[mag_roi_flat > (mag_max - 1)]))
+            kernel = blurKernel(length = mag_using, angle = ang_using)
+            blur_ro = cv2.filter2D(frame1,-1,kernel)
+            
             blurmask = emptymask.copy()
             blurmask[starty:endy, startx:endx, ...] = 255
             blurmask = cv2.filter2D(blurmask, -1, kernel)
-            # cv2.imshow('blr', blurmask)
-            # cv2.waitKey(0)
-            # mask = cv2.bitwise_or(mask, blurmask)
-            # mask = cv2.add(mask, blurmask)
-
-            res = imgAdd(res, blur_ro, blurmask)
-    
-    # mask = np.zeros_like(mag)
-    # mask[mag > 1] = 1
-    # res = cv2.GaussianBlur(res, (5, 5), 0)
-    # mask[mask > 255] = 255
-    # return mask
+            
+            res = imgAddBlur(res, blur_ro, blurmask)
+            print('rounds: {},{}'.format(in_x, in_y))
     return res
 
+def blurVideo():
+    print("start")
+    input = 'resource/testtre.mp4'
+    output = 'test.mp4'
+    cap = cv2.VideoCapture(input)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    framecount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    cacheVideoName = "cachevideo.mp4"
+    cc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(cacheVideoName, cc, fps, (width, height))
+    lastframe = np.array([])
+    currentframeindex = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        if lastframe.any():
+            thisframe = motionBlurWithFrames(lastframe, frame)
+            out.write(thisframe)
+        else:
+            out.write(frame)
+        lastframe = frame
+        print('progress:{}/{}'.format(currentframeindex, framecount))
+        currentframeindex += 1
+    cap.release()
+    out.release()
+    ffmpegcommand = "ffmpeg -i {} -i {} -map 0:v -map 1:a -c:v libx264 -c:a copy {}".format(cacheVideoName, input, output)
+    print('command: {}'.format(ffmpegcommand))
+    os.system(ffmpegcommand)
+    print("cleaning cache")
+    os.system("rm -rf {}".format(cacheVideoName))
+    print("end")
 
-
-if __name__ == "__main__":
+def testImages():
     frame1 = cv2.imread('resource/one1.png')
     frame2 = cv2.imread('resource/one2.png')
-    # size = (200,150)
-    # frame1 = cv2.resize(frame1,size)
-    # frame2 = cv2.resize(frame2,size)
-    # flow = flowWithFrames(frame1,frame2)
-
-    # showFlow(flow)
-    # testBlur(frame1)
     res = motionBlurWithFrames(frame1, frame2)
     cv2.imshow('blr', res)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    blurVideo()
+    # testImages()
